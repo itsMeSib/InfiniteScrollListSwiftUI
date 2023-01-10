5b5663f565c10fbc3ef15847bda7d70039e6dea0
itsMeSib <shahzaib.bhatti@careem.com>
2023-01-10 14:06:01 +0500

    Initial Commit

---
+++ InfiniteScrolling/InfiniteScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollingApp.swift
+//  InfiniteScrolling
+//
+//  Created by Shahzaib Iqbal Bhatti  on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
